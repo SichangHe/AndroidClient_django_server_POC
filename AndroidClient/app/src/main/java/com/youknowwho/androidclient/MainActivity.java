f118fe11832f28a49483891a28c3ee71d58e170a
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-03-12 16:42:36 +0800

    hook up main textview;add two button

--- AndroidClient/app/src/main/java/com/youknowwho/androidclient/MainActivity.java
+++ AndroidClient/app/src/main/java/com/youknowwho/androidclient/MainActivity.java
@@ -1,14 +1,22 @@
 package com.youknowwho.androidclient;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv_main;
+    private Button btn_get;
+    private Button btn_post;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_main = findViewById(R.id.tv_main);
+        btn_get = findViewById(R.id.btn_get);
+        btn_post = findViewById(R.id.btn_post);
     }
 }
